a54e1c04e61a3c5d22868e2890d8b93cba4e9baa
sourakov <surakov@avito.ru>
2016-10-13 10:30:24 +0300

    Add debug mode for core log

--- log/log.go
+++ log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"github.com/qa-dev/go-core/color"
+	"github.com/davecgh/go-spew/spew"
 	"log"
 )
 
@@ -13,4 +14,10 @@ func Info(format string, a ...interface{}) {
 func Fatal(format string, a ...interface{}) {
 	format = color.Color(color.Red, "FATAL: ")+format
 	log.Fatalf(format, a...)
+}
+
+func Debug(a... interface{}) {
+	format := color.Color(color.Yellow, "DEBUG:")
+	log.Printf(format)
+	spew.Dump(a...)
 }
